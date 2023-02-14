2a22d7730a3ebc9863aea1a4612e2e0c3aee68a4
Igor Kochergin <kocherginigorv@gmail.com>
2023-02-14 10:07:20 +0500

    1 lesson

--- main.py
+++ main.py
@@ -1,1 +1,59 @@
-print("hello world")
+##циклы
+#for i in range(6):
+#    print(i)
+
+#
+#word = "hello world"
+#for o in word:
+#    if o == "l":
+#        print("yes")
+
+#Списки
+list = [1,2,10]
+#print(list)
+#print(list[4][1])
+#list.append(True)
+#list.insert(1, False)
+#print(list)
+#b=[4,5,6]
+#list.extend(b)
+#print(list)
+#list.sort()
+#print(list)
+
+#list.pop()
+#print(list)
+#list.remove(True)
+#print(list)
+#list.pop(2)
+#print(list)
+
+#Tuple
+#Tuple = (4,6,7,9,1,6)
+#print(Tuple[1:5])
+#print(Tuple.count(6))
+#word = tuple("hello world")
+#print(word)
+
+#Dict
+#country = {'code': 'RU', 'name':'Russian','population':144}
+#country = dict(code='RU', name='Russian')
+#print(country)
+
+#for key, value in country.items():
+#    print(key, " ", value)
+#country.pop('name')
+#country.keys()
+#country.values()
+#country.items()
+#print(country)
+
+#person={
+#    'user1': {
+#        'name':'Igor',
+#        'secondName':'Kochergin',
+#        'country':'Russia'
+#    },
+#    'user2': {
+#    }
+#}
